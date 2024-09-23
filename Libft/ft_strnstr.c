6f825ab1b7f1b196226152d8c5f83e69826ab61b
AnderMartin330 <anderpas1998@hotmail.com>
2024-09-23 15:25:31 +0200

    strnstr y atoi

---
+++ Libft/ft_strnstr.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: andemart <andemart@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/20 16:54:21 by andemart          #+#    #+#             */
+/*   Updated: 2024/09/20 16:54:21 by andemart         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t h;
+	size_t n;
+
+	h = 0;
+	if (needle[h]  == '\0')  			// si mi string buscado es igual a 0
+		return ((char *)haystack);  		//retorno un puntero al string de origen
+	while (haystack[h]) 			//mientras la sring en que busco sea diferente de cero
+	{
+		n = 0;
+		while (haystack[h + n] == needle[n] && (h + n) < len)
+		{
+			if (haystack[h + n] == '\0' && needle[n] == '\0')
+				return ((char *)haystack + h);
+			n++;
+		}
+		if (needle[n] == '\0')
+			return ((char *)haystack + h);
+		h++;
+	}
+	return (0);
+}
