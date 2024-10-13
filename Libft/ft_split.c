d561d3d4542bba86b659d67c1eb0e90010a70533
Gothep <anderpas1998@hotmail.com>
2024-10-13 12:40:09 +0000

    split arreglos y cambio de nombre putchar

--- Libft/ft_split.c
+++ Libft/ft_split.c
@@ -6,55 +6,39 @@
 /*   By: andemart <andemart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 16:57:13 by andemart          #+#    #+#             */
-/*   Updated: 2024/10/06 17:28:20 by andemart         ###   ########.fr       */
+/*   Updated: 2024/10/13 12:29:32 by andemart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static size_t	ft_countword(char const *s, char c)
-{
-	size_t	count;
-
-	if (!*s)
-		return (0);
-	count = 0;
-	while (*s)
-	{
-		while (*s == c)
-			s++;
-		if (*s)
-			count++;
-		while (*s != c && *s)
-			s++;
-	}
-	return (count);
-}
-
 char	**ft_split(char const *s, char c)
 {
-	char **lst;
-	size_t word_len;
-	int i;
+	char	**lst;
+	int		str_index;
+	int		array_index;
+	int		array_str_index;
 
-	lst = (char **)malloc((ft_countword(s, c) + 1) * sizeof(char *));
-	if (!s || !lst)
+	str_index = 0;
+	array_index = 0;
+	array_str_index = 0;
+	
+	if (!s)
 		return (NULL);
-	i = 0;
-	while (*s)
-	{
-		while (*s == c && *s)
-			s++;
-		if (*s)
-		{
-			if (!ft_strchr(s, c))
-				word_len = ft_strlen(s);
-			else
-				word_len = ft_strchr(s, c) - s;
-			lst[i++] = ft_substr(s, 0, word_len);
-			s += word_len;
-		}
+	lst = (char **)malloc((ft_strlen(s) + 1) * sizeof(char *));
+	while(s[str_index])
+    {
+        if(s[str_index] == c)
+        {
+            lst[array_index][array_str_index] = '\0';
+            array_index++;
+            str_index++;
+            array_str_index = 0;
+            continue;
+        }
+		lst[array_index][array_str_index] = s[str_index];
+        str_index++;
 	}
-	lst[i] = NULL;
+	lst[array_index++] = NULL;
 	return (lst);
 }
